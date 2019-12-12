d7dd6e59a9f02bd75e64ab2beffd6dd2cd1687e5
aboutmydreams <aboutmydreams@163.com>
2019-12-12 17:45:27 +0800

    :heavy_plus_sign: :sparkles: Building the framework

---
+++ lib/routes/top_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:yuyan_app/views/home_page.dart';
+
+Map<String, WidgetBuilder> routeData = {
+  '/': (context) => HomePage(),
+  // '/first': (context) => FirstPage(),
+  // '/login': (context) => LoginPage(),
+  // '/door': (context) => DoorPage(),
+  // '/setting': (context) => SettingPage(),
+  // '/aboutwork': (context) => AboutWorkPage(),
+  // '/addtopic': (context) => AddTopicPage(),
+  // '/my': (context) => MyPage(),
+  // '/addcomment': (context) => AddComment(),
+};
