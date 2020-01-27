8c2b7901407526c9e60cfc4d396a64bafecb4a3a
aboutmydreams <aboutmydreams@163.com>
2020-01-28 05:02:00 +0800

    :whale: add theme manege

--- lib/state_manage/toppest.dart
+++ lib/state_manage/toppest.dart
@@ -1,7 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:yuyan_app/models/tools/get_pref.dart';
 import 'package:yuyan_app/state_manage/account/color_manage.dart';
 import 'package:yuyan_app/state_manage/dataManage/attent_manage.dart';
 import 'package:yuyan_app/state_manage/dataManage/mydata_manage.dart';
@@ -12,94 +8,6 @@ import 'package:yuyan_app/state_manage/dataManage/select_manage.dart';
 
 class TopStateModel extends ColorManage {
   // ColorManage colorManage = ColorManage();
-  // static List<MaterialColor> _colorList = [
-  //   Colors.yellow,
-  //   Colors.orange,
-  //   Colors.deepOrange,
-  //   Colors.amber,
-  //   Colors.blue,
-  //   Colors.red,
-  //   Colors.pink,
-  //   Colors.lime,
-  //   Colors.lightGreen,
-  //   Colors.green,
-  //   Colors.teal,
-  //   Colors.cyan,
-  //   Colors.indigo,
-  //   Colors.purple,
-  //   Colors.deepPurple,
-  //   Colors.grey,
-  //   Colors.blueGrey,
-  //   Colors.brown,
-  // ];
-
-  // int nowColor;
-  // MaterialColor _primarySwatchColor;
-  // MaterialColor get primarySwatchColor => _primarySwatchColor;
-
-  // ThemeData _themeData;
-  // ThemeData get themeData => _themeData;
-
-  // static saveColor(int colorNum) async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   prefs.setInt("primarySwatchColor", colorNum);
-  //   return 1;
-  // }
-
-  // getMyColor() async {
-  //   var _myColor = await getPrefIntData("primarySwatchColor");
-  //   _myColor ??= 0;
-  //   _primarySwatchColor = _colorList[_myColor];
-  //   notifyListeners();
-  //   return _myColor;
-  // }
-
-  // getMyTheme() {
-  //   _themeData = ThemeData(
-  //     platform: TargetPlatform.iOS, // 右滑返回
-  //     primarySwatch: _primarySwatchColor,
-  //     highlightColor: Color.fromRGBO(255, 255, 255, 0.9),
-  //     splashColor: Colors.yellow,
-  //     accentColor: Color.fromRGBO(25, 25, 25, 1.0),
-  //   );
-  //   print("theme");
-  //   notifyListeners();
-  //   return 1;
-  // }
-
-  // void changeColor() {
-  //   getMyColor().then((res) {
-  //     var nowColor = res;
-  //     nowColor++;
-
-  //     if (nowColor == null) {
-  //       nowColor = 0;
-  //       saveColor(0);
-  //     }
-  //     if (nowColor == _colorList.length) {
-  //       nowColor = 0;
-  //     }
-
-  //     _primarySwatchColor = _colorList[nowColor];
-  //     print(_primarySwatchColor);
-
-  //     // Sava the color num
-  //     saveColor(nowColor).then((res) async {
-  //       print("saveNowColor===");
-  //       print(nowColor);
-  //       print("saveNowColor==");
-  //       getMyTheme();
-  //       notifyListeners();
-  //       return 1;
-  //     }).then((res) {
-  //       notifyListeners();
-  //       print("noti again");
-  //     });
-  //     // Then notify all the listeners.
-  //     notifyListeners();
-  //     print("notifyListeners!!!");
-  //   });
-  // }
 
   SelectManage selectManage = SelectManage();
   AttentManage attentManage = AttentManage();
