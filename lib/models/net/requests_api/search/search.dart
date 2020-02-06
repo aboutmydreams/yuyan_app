786d3bdfde9ffc5dcae513e6d105f8e748eaf56c
aboutmydreams <aboutmydreams@163.com>
2020-02-06 17:11:33 +0800

    add search api

--- lib/models/net/requests_api/search/search.dart
+++ lib/models/net/requests_api/search/search.dart
@@ -9,38 +9,53 @@ import 'data/user_data.dart';
 
 class DioSearch {
   // 搜索文档
-  static getDoc(String text, int page) async {
-    var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=doc");
+  static getDoc({String text, int page, bool aboutMe: false}) async {
+    var url = "/zsearch?p=$page&q=$text&scope=&type=doc";
+    url = aboutMe ? url + "&related=true" : url;
+    var ans = await DioReq.get(url);
     return SearchDocJson.fromJson(ans);
   }
 
   // 搜索知识库
-  static getRepos(String text, int page) async {
-    var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=book");
+  static getRepos({String text, int page, bool aboutMe: false}) async {
+    var url = "/zsearch?p=$page&q=$text&scope=&type=book";
+    url = aboutMe ? url + "&related=true" : url;
+    var ans = await DioReq.get(url);
     return SearchReposJson.fromJson(ans);
   }
 
   // 搜索画板
-  static getArt(String text, int page) async {
-    var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=artboard");
+  static getArt({String text, int page, bool aboutMe: false}) async {
+    var url = "/zsearch?p=$page&q=$text&scope=&type=artboard";
+    url = aboutMe ? url + "&related=true" : url;
+    var ans = await DioReq.get(url);
     return SearchArtboardJson.fromJson(ans);
   }
 
   // 搜索讨论
-  static getTopic(String text, int page) async {
-    var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=topic");
+  static getTopic({String text, int page, bool aboutMe: false}) async {
+    var url = "/zsearch?p=$page&q=$text&scope=&type=topic";
+    url = aboutMe ? url + "&related=true" : url;
+    var ans = await DioReq.get(url);
     return SearchTopicJson.fromJson(ans);
   }
 
-  // 搜索团队
-  static getGroup(String text, int page) async {
+  // 搜索团队(默认与我无关，这个接口也可以搜索有关，但是没有必要)
+  static getGroup({String text, int page}) async {
     var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=group");
     return SearchGroupJson.fromJson(ans);
   }
 
-  // 搜索用户
+  // 搜索用户(默认与我无关)
   static getUser(String text, int page) async {
     var ans = await DioReq.get("/zsearch?p=$page&q=$text&scope=&type=user");
+    return SearchUserJson.fromJson(ans);
+  }
+
+  // 搜索附件(默认与我相关)
+  static getAttachment({String text, int page}) async {
+    var ans = await DioReq.get(
+        "/zsearch?p=$page&q=$text&scope=&type=attachment&related=true");
     return SearchUserJson.fromJson(ans);
   }
 }
