3046a4865cc534e2205383a33bbce2219f6f0286
aboutmydreams <aboutmydreams@163.com>
2020-02-11 11:14:17 +0800

    :star: hide the bottom bar but some bug come in guai point

--- lib/state_manage/layout_manage/hide_bottom.dart
+++ lib/state_manage/layout_manage/hide_bottom.dart
@@ -33,8 +33,7 @@ class BottomManage extends Model {
   }
 
   bool isFirst() {
-    // var difference = firstTime.difference(thatTime);
-    return firstCome;
+    return firstCome && !_hideBottomBar;
   }
 
   void addPixels(double d) {
@@ -51,10 +50,10 @@ class BottomManage extends Model {
         d3 = d;
       } else if ((d3 > d2 && d3 > d) || (d3 < d2 && d3 < d)) {
         // 拐点
-        print("拐点来了！");
+        print("拐点来了！$_hideBottomBar");
         _firstY = d;
-        firstCome = true;
-        notifyListeners();
+        // firstCome = true;
+
         _nowY = d;
         d1 = d2;
         d2 = d3;
@@ -66,20 +65,53 @@ class BottomManage extends Model {
         d3 = d;
         // 与第一次计算差值
         var changeY = d - _firstY;
-
-        if (changeY > 56) {
-          _y = 0;
-          // 第一次完全隐藏
-          _hideBottomBar = true;
-          // _firstY = _nowY;
-        } else if ((56 > changeY) && (changeY > 0) && (!_hideBottomBar)) {
-          _y = changeY;
-        } else if ((-56 < changeY) && (changeY < 0)) {
-          _y = -changeY;
+        if (!_hideBottomBar) {
+          // 完全没有隐藏
+          print("完全没有隐藏changeY");
+          if (changeY <= -56) {
+            _y = 56;
+            _firstY = d;
+          } else if ((-56 <= changeY) && (changeY <= 0)) {
+            // _y = -changeY;
+            _y = 56;
+          } else if ((56 >= changeY) && (changeY > 0)) {
+            _y = 56 - changeY;
+          } else {
+            _y = 0;
+            _hideBottomBar = true;
+          }
         } else {
-          _y = 56;
-          _hideBottomBar = false;
+          print("完全隐藏$changeY");
+          if (changeY >= 56) {
+            _y = 0;
+            _firstY = d;
+          } else if ((-56 <= changeY) && (changeY <= 0)) {
+            _y = -changeY;
+          } else if ((56 >= changeY) && (changeY > 0)) {
+            // _y = 56 - changeY;
+            _y = 0;
+          } else {
+            _y = 56;
+            _hideBottomBar = false;
+          }
         }
+
+        // if (changeY > 56) {
+        //   _y = 0;
+        //   // 第一次完全隐藏
+        //   _hideBottomBar = true;
+        //   // _firstY = _nowY;
+        // } else if ((56 >= changeY) && (changeY > 0)) {
+        //   _y = 56 - changeY;
+        // } else if ((-56 <= changeY) && (changeY <= 0)) {
+        //   _y = -changeY;
+        // } else if (changeY < -56) {
+        //   _y = 56;
+        //   // print("来了56");
+        //   _hideBottomBar = false;
+        // } else {
+        //   print("什么情况$changeY");
+        // }
       }
     }
 
