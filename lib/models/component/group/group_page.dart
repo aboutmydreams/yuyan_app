0dca9689b2edd42136e581067df85c3731944e5e
aboutmydreams <aboutmydreams@163.com>
2020-02-13 07:24:28 +0800

    :star: add group page - hero and tabView

--- lib/models/component/group/group_page.dart
+++ lib/models/component/group/group_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:yuyan_app/models/component/appUI.dart';
 import 'package:yuyan_app/models/widgets_small/user_avatar.dart';
 import 'package:yuyan_app/state_manage/dataManage/data/my_page/group/group_data.dart';
+import 'package:yuyan_app/views/explore_page/selection/selection_page.dart';
 
 class GroupPage extends StatefulWidget {
   GroupPage({Key key, this.groupdata, this.pageIndex}) : super(key: key);
@@ -25,9 +27,11 @@ class _GroupPageState extends State<GroupPage> {
       "讨论区",
       "成员",
     ];
+    print(pageIndex);
 
     return Scaffold(
       body: DefaultTabController(
+        initialIndex: pageIndex,
         length: _tabs.length, // This is the number of tabs.
         child: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
@@ -49,8 +53,11 @@ class _GroupPageState extends State<GroupPage> {
                   floating: false,
                   snap: false,
                   primary: true,
-                  expandedHeight: 230.0,
 
+                  expandedHeight: 460.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0)),
+                  stretchTriggerOffset: 10,
                   elevation: 10,
                   //是否显示阴影，直接取值innerBoxIsScrolled，展开不显示阴影，合并后会显示
                   forceElevated: innerBoxIsScrolled,
@@ -92,6 +99,7 @@ class _GroupPageState extends State<GroupPage> {
                   ),
 
                   bottom: TabBar(
+                    indicatorColor: Colors.white54,
                     tabs: _tabs.map((String name) => Tab(text: name)).toList(),
                   ),
                 ),
@@ -100,15 +108,40 @@ class _GroupPageState extends State<GroupPage> {
           },
           body: TabBarView(
             // These are the contents of the tab views, below the tabs.
-            children: _tabs.map((String name) {
-              //SafeArea 适配刘海屏的一个widget
-              return SafeArea(
+            children: [
+              SafeArea(
+                top: false,
+                bottom: false,
+                child: CustomScrollView(
+                  slivers: <Widget>[
+                    // SliverOverlapInjector(
+                    //   handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                    //       context),
+                    // ),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(10.0),
+                      sliver: SliverFixedExtentList(
+                        itemExtent: 50.0, //item高度或宽度，取决于滑动方向
+                        delegate: SliverChildBuilderDelegate(
+                          (BuildContext context, int index) {
+                            return ListTile(
+                              title: Text('Item $index'),
+                            );
+                          },
+                          childCount: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SafeArea(
                 top: false,
                 bottom: false,
                 child: Builder(
                   builder: (BuildContext context) {
                     return CustomScrollView(
-                      key: PageStorageKey<String>(name),
+                      // key: PageStorageKey<String>(name),
                       slivers: <Widget>[
                         SliverOverlapInjector(
                           handle:
@@ -133,8 +166,27 @@ class _GroupPageState extends State<GroupPage> {
                     );
                   },
                 ),
-              );
-            }).toList(),
+              ),
+              CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.all(10.0),
+                    sliver: SliverFixedExtentList(
+                      itemExtent: 50.0, //item高度或宽度，取决于滑动方向
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          return ListTile(
+                            title: Text('Item $index'),
+                          );
+                        },
+                        childCount: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SelectionPage(),
+            ],
           ),
         ),
       ),
