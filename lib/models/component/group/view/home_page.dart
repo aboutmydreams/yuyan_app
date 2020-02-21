c8f8f70ebb06d43fac4aebd1d4a308810a75ded4
aboutmydreams <aboutmydreams@163.com>
2020-02-21 19:06:06 +0800

    :star: finish the topic page in group

--- lib/models/component/group/view/home_page.dart
+++ lib/models/component/group/view/home_page.dart
@@ -2,35 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:yuyan_app/models/component/appUI.dart';
 import 'package:yuyan_app/models/component/web/open_url.dart';
 import 'package:yuyan_app/models/net/requests_api/group/data/group_home_data.dart';
-import 'package:yuyan_app/models/net/requests_api/group/data/group_home_data.dart';
-import 'package:yuyan_app/models/tools/clear_text.dart';
-import 'package:yuyan_app/models/widgets_small/list_animation.dart';
 import 'package:yuyan_app/models/widgets_small/loading.dart';
 import 'package:yuyan_app/models/widgets_small/nothing.dart';
 import 'package:yuyan_app/models/widgets_small/user_avatar.dart';
 
-class GroupHome extends StatefulWidget {
+class GroupHome extends StatelessWidget {
   GroupHome({Key key, this.homeJson}) : super(key: key);
   final GroupHomeJson homeJson;
 
   @override
-  _GroupHomeState createState() => _GroupHomeState(homeJson: homeJson);
-}
-
-class _GroupHomeState extends State<GroupHome> {
-  _GroupHomeState({Key key, this.homeJson});
-  GroupHomeJson homeJson;
-
-  @override
   Widget build(BuildContext context) {
-    print(homeJson.data.bookStacks);
+    // print(homeJson.data.bookStacks);
     return homeJson == null
         ? loading()
         : homeJson.data.bookStacks.isEmpty
             ? NothingPage()
             : SingleChildScrollView(
-                child: aniColumn(
-                  aniWhich: 4,
+                child: Column(
                   children: [SizedBox(height: 155)]
                     ..addAll(homeJson.data.bookStacks.map((a) {
                       return oneHome(context, a);
@@ -41,13 +29,29 @@ class _GroupHomeState extends State<GroupHome> {
 }
 
 Widget oneHome(BuildContext context, BookStacks data) {
+  return Container(
+    child: Column(
+      children: <Widget>[
+        Text(
+          "${data.name}",
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: AppStyles.textStyleB,
+        ),
+      ]..addAll(data.books.map((b) {
+          return oneBook(context, b);
+        })),
+    ),
+  );
+}
+
+Widget oneBook(BuildContext context, Books data) {
   print(data.name);
   return GestureDetector(
     onTap: () {
       // openUrl(context, "https://www.yuque.com/${data.login}");
     },
     child: Container(
-      height: 70,
       margin: EdgeInsets.only(left: 15, top: 2, bottom: 8, right: 15),
       decoration: BoxDecoration(
         color: AppColors.background,
@@ -67,21 +71,20 @@ Widget oneHome(BuildContext context, BookStacks data) {
           // userAvatar(data.user.avatarUrl, height: 50),
           Container(
             margin: EdgeInsets.only(left: 6),
-            child: iconType[data.displayType],
+            child: iconType[data.type],
           ),
           Container(
-              width: MediaQuery.of(context).size.width * 0.4,
-              margin: EdgeInsets.only(left: 20),
-              child: Container(
-                child: Text(
-                  "${data.name}",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: AppStyles.textStyleB,
-                ),
-              )),
-
-          // FollowButtom(data: data)
+            width: MediaQuery.of(context).size.width * 0.4,
+            margin: EdgeInsets.only(left: 20),
+            child: Container(
+              child: Text(
+                "${data.name}",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: AppStyles.textStyleB,
+              ),
+            ),
+          ),
         ],
       ),
     ),
--- lib/models/component/group/view/topic_page.dart
+++ lib/models/component/group/view/topic_page.dart
@@ -3,6 +3,7 @@ import 'package:yuyan_app/models/component/appUI.dart';
 import 'package:yuyan_app/models/component/web/open_url.dart';
 import 'package:yuyan_app/models/net/requests_api/group/data/group_topic_data.dart';
 import 'package:yuyan_app/models/tools/clear_text.dart';
+import 'package:yuyan_app/models/tools/time_cut.dart';
 import 'package:yuyan_app/models/widgets_small/list_animation.dart';
 import 'package:yuyan_app/models/widgets_small/loading.dart';
 import 'package:yuyan_app/models/widgets_small/nothing.dart';
@@ -47,62 +48,80 @@ Widget oneTopic(BuildContext context, OneTopicData data) {
       // openUrl(context, "https://www.yuque.com/${data.login}");
     },
     child: Container(
-      height: 70,
-      margin: EdgeInsets.only(left: 15, top: 2, bottom: 8, right: 15),
-      decoration: BoxDecoration(
-        color: AppColors.background,
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(25, 0, 0, 0),
-            offset: Offset(1, 2),
-            blurRadius: 4,
-          ),
-        ],
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(width: 20),
-          userAvatar(data.user.avatarUrl, height: 50),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.4,
-            margin: EdgeInsets.only(left: 20),
-            child: data.user.description != null
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          clearText(data.user.name, 10),
-                          style: AppStyles.textStyleB,
-                        ),
+        height: 70,
+        margin: EdgeInsets.only(left: 15, top: 2, bottom: 8, right: 15),
+        padding: EdgeInsets.only(left: 15, top: 12, bottom: 10, right: 15),
+        decoration: BoxDecoration(
+          color: AppColors.background,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(25, 0, 0, 0),
+              offset: Offset(1, 2),
+              blurRadius: 4,
+            ),
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        child: Row(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    clearText(data.title, 15),
+                    style: AppStyles.textStyleB,
+                  ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    userAvatar(data.user.avatarUrl, height: 20),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      margin: EdgeInsets.only(left: 12),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 2),
+                          Container(
+                            child: Text(
+                              "${data.user.name} 创建于 ${timeCut(data.createdAt)}",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppStyles.textStyleC,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 2),
-                      Container(
-                        child: Text(
-                          "${clearText(data.user.description, 15)}",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppStyles.textStyleC,
-                        ),
-                      ),
-                    ],
-                  )
-                : Container(
-                    child: Text(
-                      "${data.user.name}",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppStyles.textStyleB,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Spacer(),
+            Container(
+              margin: EdgeInsets.only(right: 4),
+              width: 45,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Icon(
+                      Icons.question_answer,
+                      size: 19,
                     ),
                   ),
-          ),
-          Spacer(),
-          // FollowButtom(data: data)
-        ],
-      ),
-    ),
+                  SizedBox(width: 7),
+                  Text("${data.commentsCount}")
+                ],
+              ),
+            )
+          ],
+        )),
   );
 }
