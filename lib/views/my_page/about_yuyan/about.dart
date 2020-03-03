e31b1607b9cb4e1b1ea965371658a7136cce1b9d
aboutmydreams <aboutmydreams@163.com>
2020-03-03 23:51:29 +0800

    add about yuyan page

---
+++ lib/views/my_page/about_yuyan/about.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutYuyan extends StatefulWidget {
+  AboutYuyan({Key key}) : super(key: key);
+
+  @override
+  _AboutYuyanState createState() => _AboutYuyanState();
+}
+
+class _AboutYuyanState extends State<AboutYuyan> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView();
+  }
+}
