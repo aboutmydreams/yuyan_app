0cedcf2cc1eca5aa7a87e595b0bdc0ffac940e3f
aboutmydreams <aboutmydreams@163.com>
2020-03-11 13:19:14 +0800

    :bug: fix a change color bug in my page

--- lib/views/my_page/my_page.dart
+++ lib/views/my_page/my_page.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:yuyan_app/state_manage/toppest.dart';
 import 'package:yuyan_app/views/my_page/view/bottom_column.dart';
 import 'package:yuyan_app/views/my_page/view/user_info.dart';
 
-class MyPage extends StatefulWidget {
+class MyPage extends StatelessWidget {
   MyPage({Key key}) : super(key: key);
-
-  @override
-  _MyPageState createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
-  ScrollController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -29,26 +23,30 @@ class _MyPageState extends State<MyPage> {
             top: 0,
             child: ClipPath(
               clipper: ArcClipper(),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.33,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: topModel.primarySwatchColor,
-                  gradient: LinearGradient(
-                      colors: [
-                        topModel.primarySwatchColor,
-                        topModel.primarySwatchColor.withAlpha(60)
+              child: ScopedModelDescendant<TopStateModel>(
+                builder: (context, child, model) {
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.33,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: model.primarySwatchColor,
+                      gradient: LinearGradient(
+                          colors: [
+                            model.primarySwatchColor,
+                            model.primarySwatchColor.withAlpha(60)
+                          ],
+                          begin: FractionalOffset(0, 0),
+                          end: FractionalOffset(0, 1)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(55, 0, 0, 0),
+                          offset: Offset(1, 2),
+                          blurRadius: 4,
+                        ),
                       ],
-                      begin: FractionalOffset(0, 0),
-                      end: FractionalOffset(0, 1)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromARGB(55, 0, 0, 0),
-                      offset: Offset(1, 2),
-                      blurRadius: 4,
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           ),
@@ -59,7 +57,7 @@ class _MyPageState extends State<MyPage> {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height - 56,
               child: ListView.builder(
-                  controller: _controller,
+                  // controller: _controller,
                   itemCount: widgetList.length,
                   itemBuilder: (BuildContext context, int index) {
                     return widgetList[index];
