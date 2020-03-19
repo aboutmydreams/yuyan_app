4ae5cd6510fd0f8b9a1eb9cb9ee5d7923ba5f24b
aboutmydreams <aboutmydreams@163.com>
2020-03-19 10:59:29 +0800

    :bug: fix login black scream

--- lib/views/begin_init/guide_page/guide_page.dart
+++ lib/views/begin_init/guide_page/guide_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:yuyan_app/views/begin_init/guide_page/guide1.dart';
 import 'package:yuyan_app/views/begin_init/guide_page/guide2.dart';
 import 'package:yuyan_app/views/begin_init/guide_page/guide3.dart';
+import 'package:yuyan_app/views/begin_init/login_page/login_page.dart';
 
 class GuidePage extends StatelessWidget {
   int n = 0;
@@ -12,15 +13,16 @@ class GuidePage extends StatelessWidget {
     Container(child: Guide1()),
     Container(child: Guide2()),
     Container(child: Guide3()),
+    // Container(child: LoginPage())
   ];
 
   pageChangeCallback(BuildContext context, int page) {
     n += 1;
-    if ((n >= 3) & (page == 2)) {
-      print(n);
-      Navigator.pushNamedAndRemoveUntil(
-          context, "/login", (route) => route == null);
-    }
+    // if ((n > 3) & (page == 2)) {
+    //   print(n);
+    //   Navigator.pushNamedAndRemoveUntil(
+    //       context, "/login", (route) => route == null);
+    // }
   }
 
   updateTypeCallback(UpdateType updateType) {
