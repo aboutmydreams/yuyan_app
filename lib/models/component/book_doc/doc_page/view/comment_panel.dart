418fab867e2cccb5b59190c68f0f1ade3f274418
aboutmydreams <aboutmydreams@163.com>
2020-04-06 17:54:24 +0800

    :hammer: all webview in doc page context

--- lib/models/component/book_doc/doc_page/view/comment_panel.dart
+++ lib/models/component/book_doc/doc_page/view/comment_panel.dart
@@ -1,67 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:yuyan_app/models/component/appUI.dart';
+import 'package:yuyan_app/models/component/group/topic/view/the_comment.dart';
+import 'package:yuyan_app/models/net/requests_api/doc/data/comments_data.dart';
 import 'package:yuyan_app/models/widgets_small/text_field.dart';
 
 class CommentPanel extends StatelessWidget {
-  const CommentPanel(
-      {Key key,
-      this.textControl,
-      this.panelControl,
-      this.onpressed,
-      this.autofocus: false})
-      : super(key: key);
+  const CommentPanel({
+    Key key,
+    this.textControl,
+    this.panelControl,
+    this.onpressed,
+    this.comments,
+    this.scrollController,
+    this.autofocus: false,
+  }) : super(key: key);
   final TextEditingController textControl;
   final PanelController panelControl;
   final Function onpressed;
   final bool autofocus;
+  final Comments comments;
+  final scrollController;
 
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     bool nowfocus = autofocus;
+    print(comments.data.length);
     return Scaffold(
       body: Container(
-        height: 130,
         margin: EdgeInsets.only(top: 13),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(left: 24),
+            Positioned(
+              top: 0,
+              left: 16,
               child: Text(
-                "发表评论",
-                style: AppStyles.textStyleBBB,
+                "评论",
+                style: AppStyles.textStyleBp,
               ),
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: w * 0.79,
-                  padding: EdgeInsets.fromLTRB(w * 0.02, 12, w * 0.02, 12),
-                  child: CommentTextfierd(
-                    controller: textControl,
-                    w: w * 0.76,
-                    autofocus: nowfocus,
-                  ),
+            Positioned(
+              top: 25,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.only(left: 16),
+                child: ListView.builder(
+                  itemCount: comments.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return oneComment(context, comments.data[index]);
+                  },
                 ),
-                Container(
-                  height: 100,
-                  child: Column(
+              ),
+            ),
+            Positioned(
+              // top: 20,
+              bottom: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Container(
-                        width: w * 0.20,
-                        margin: EdgeInsets.only(left: w * 0.003),
-                        child: FlatButton.icon(
-                          onPressed: () {
-                            panelControl.close().then((value) {
-                              nowfocus = false;
-                            });
-                          },
-                          icon: Icon(Icons.swap_vert),
-                          label: Text("收起", style: AppStyles.textStyleBBB),
-                          padding: EdgeInsets.all(0),
+                        width: w * 0.79,
+                        padding:
+                            EdgeInsets.fromLTRB(w * 0.02, 12, w * 0.02, 12),
+                        child: CommentTextfierd(
+                          controller: textControl,
+                          w: w * 0.76,
+                          maxLines: 1,
+                          autofocus: nowfocus,
+                          hintText: " 说点什么吧⋯",
                         ),
                       ),
                       Container(
@@ -71,18 +83,20 @@ class CommentPanel extends StatelessWidget {
                           autofocus: true,
                           color: AppColors.primary,
                           textColor: Colors.white.withAlpha(235),
-                          child: Text("发表",
-                              style: TextStyle(
-                                fontFamily: "sans_bold",
-                                fontWeight: FontWeight.w400,
-                                fontSize: 13,
-                              )),
+                          child: Text(
+                            "发表",
+                            style: TextStyle(
+                              fontFamily: "sans_bold",
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13,
+                            ),
+                          ),
                         ),
                       )
                     ],
                   ),
-                )
-              ],
+                ],
+              ),
             ),
           ],
         ),
