621e44b95b3d1be33cae9d4753c10748ca652b06
aboutmydreams <aboutmydreams@163.com>
2020-06-13 00:30:31 +0800

    :zap: rename the doc book

---
+++ lib/models/net/requests_api/doc_book/book.dart
@@ -0,0 +1,12 @@
+import 'package:yuyan_app/models/net/requests/dio_requests.dart';
+import 'package:yuyan_app/models/net/requests_api/doc_book/data/doc_book_data.dart';
+
+class DioBook {
+  // 文档知识库
+  static getData({int bookId}) async {
+    var res = await DioReq.get(
+        "/books/$bookId/docs?include_contributors=true&include_hits=true&limit=200&offset=0");
+    DocBookJson theData = DocBookJson.fromJson(res);
+    return theData;
+  }
+}
