7dc0884d0db6b231b7ff7b47fa8ea8f5f6bd6ba8
aboutmydreams <aboutmydreams@163.com>
2020-10-06 00:00:28 +0800

    fix: :bug: fix Incorrect use of ParentDataWidget in my bookdoc page

--- lib/models/component/book_doc/view/one_my_doc.dart
+++ lib/models/component/book_doc/view/one_my_doc.dart
@@ -7,32 +7,30 @@ import 'package:yuyan_app/models/widgets_small/user_avatar.dart';
 
 Widget autoText(
     {String title, String des, String user, String userImg, int maxLines: 2}) {
-  return Expanded(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          clearText(title, 10),
-          style: AppStyles.textStyleB,
-        ),
-        SizedBox(height: 2),
-        Text(
-          des,
-          style: AppStyles.textStyleC,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-        ),
-        SizedBox(height: 4),
-        Row(
-          children: [
-            userAvatar(userImg, height: 22),
-            SizedBox(width: 5),
-            Text(user, style: AppStyles.textStyleC),
-          ],
-        )
-      ],
-    ),
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        clearText(title, 10),
+        style: AppStyles.textStyleB,
+      ),
+      SizedBox(height: 2),
+      Text(
+        des,
+        style: AppStyles.textStyleC,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 2,
+      ),
+      SizedBox(height: 4),
+      Row(
+        children: [
+          userAvatar(userImg, height: 22),
+          SizedBox(width: 5),
+          Text(user, style: AppStyles.textStyleC),
+        ],
+      )
+    ],
   );
 }
 
@@ -70,11 +68,13 @@ Widget buildDoc(BuildContext context, Map data) {
           ? Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                autoText(
-                  title: data['title'],
-                  userImg: data["avatar"],
-                  des: data['description'],
-                  user: data['user'],
+                Expanded(
+                  child: autoText(
+                    title: data['title'],
+                    userImg: data["avatar"],
+                    des: data['description'],
+                    user: data['user'],
+                  ),
                 ),
                 Container(
                   margin: EdgeInsets.only(left: 10),
